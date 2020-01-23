73220eacf9ea587d084dede2be071178e4e4f4ac
Vasya Peregarkin <slava.kovazin@gmail.com>
2020-01-23 21:32:54 +0500

    Updates

---
+++ precache-manifest.4fb434fcda229a2b837752b4b0cf8400.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b5c0151b4020136df6b31314c9a02c7",
+    "url": "/countdown-timer/index.html"
+  },
+  {
+    "revision": "2a3f4d70f9d6a2881783",
+    "url": "/countdown-timer/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "083ac968a9b7ea4233c4",
+    "url": "/countdown-timer/static/css/main.af94ffbf.chunk.css"
+  },
+  {
+    "revision": "2a3f4d70f9d6a2881783",
+    "url": "/countdown-timer/static/js/2.3f5c69a3.chunk.js"
+  },
+  {
+    "revision": "0d51a26e357591260d29df3c0b1559d1",
+    "url": "/countdown-timer/static/js/2.3f5c69a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "083ac968a9b7ea4233c4",
+    "url": "/countdown-timer/static/js/main.56b7ab2f.chunk.js"
+  },
+  {
+    "revision": "37dafe596882eb35348d",
+    "url": "/countdown-timer/static/js/runtime-main.1ae1725d.js"
+  }
+]);
